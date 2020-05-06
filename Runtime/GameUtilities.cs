b5975e0b6ef25ffebb97bf7b397deab27da86517
OscarGutierrezT <oscar_a1992@hotmail.com>
2020-05-06 09:12:10 -0500

    Update GameUtilities.cs

--- Runtime/GameUtilities.cs
+++ Runtime/GameUtilities.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace Roxy.core
+namespace Ezphera.QueryTK
 {
     public class GameUtilities : MonoBehaviour
     {
